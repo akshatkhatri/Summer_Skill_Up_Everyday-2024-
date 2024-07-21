e8bac5cf38a996f80d9936d8bba251fab57be2d3
Akshat Khatri <jitikhatri@gmail.com>
2024-07-21 23:10:30 +0530

    Day20

---
+++ Day20/objects.js
@@ -0,0 +1,18 @@
+const myself = {
+    name : "Akshat Khatri",
+    age : 19,
+    isme : true,
+    awesome_meter : 10,
+    pet :"Ally",
+    siblings : ["Ishita","Anshul"],
+    saymyname : function(){
+        console.log("Akshat Khatri"+" Age is "+this.age)
+    }
+};
+
+console.log(myself);
+console.log(myself.saymyname());
+
+let random = Math.random() * 100
+Pi = Math.PI
+console.log(random);
